3b18b5c7731ac07bc6a4a92ea9c974fbc2ecd9f5
Matthieu Viry <matthieu.viry@cnrs.fr>
2023-07-04 16:26:29 +0200

    Add some helper functions

--- src/index.ts
+++ src/index.ts
@@ -49,7 +49,7 @@ enum Provider {
  * @property {string} name - Palette name
  * @property {number} number - Palette number
  * @property {PaletteType} type - Palette type
- * @property {string[]} colors - Palette colors
+ * @property {string[]} colors - Palette colors (hexadecimal)
  * @property {Provider} provider - Palette provider
  * @property {string} [url] - Palette url
  */
@@ -65,11 +65,72 @@ type Palette = {
 
 const palettes = data as Palette[];
 
-export const getPalette = (name: string, number: number, reverse?: boolean): string[] | undefined => {
-  const pal = palettes.find((palette) => palette.name === name && palette.number === number);
+/**
+ * @description Get a palette.
+ * @param {string} name - Palette name
+ * @param {number} number - Number of classes in the palette
+ * @returns {Palette} - The corresponding palette
+ */
+export const getPalette = (name: string, number: number): Palette | undefined => palettes.find((palette) => palette.name === name && palette.number === number);
+
+/**
+ * @description Get the colors of a palette.
+ * @param {string} name - Palette name
+ * @param {number} number - Number of classes in the palette
+ * @param {boolean} reverse - Whether to reverse the order of the colors
+ */
+export const getColors = (name: string, number: number, reverse?: boolean): string[] | undefined => {
+  const pal = getPalette(name, number);
   if (!pal) return;
   if (reverse === true) {
     return pal.colors.slice().reverse();
   }
   return pal.colors;
 }
+
+/**
+ * @description Find palettes matching the requested criteria. Note that 1) if no criteria is provided, all palettes are returned,
+ * 2) if multiple criteria are provided, they are combined with AND, and 3) the criteria are case-insensitive.
+ * @param {Object} [options] - Options
+ * @param {string} [options.type] - Palette type ('sequential', 'diverging' or 'qualitative')
+ * @param {number} [options.number] - Number of classes in the palette
+ * @param {string} [options.provider] - Palette provider
+ * @param {string} [options.name] - Palette name
+ */
+export const findPalettes = (options: { type?: string, number?: number, provider?: string, name?: string} = {}): Palette[] => {
+  const { type, number, provider, name } = options;
+
+  // If there is no criteria, return all palettes.
+  if (!type && !number && !provider && !name) return palettes;
+
+  const _type = type ? type.toLowerCase() : undefined;
+  const _provider = provider ? provider.toLowerCase() : undefined;
+  const _name = name ? name.toLowerCase() : undefined;
+
+  // Find palettes that match the requested criteria.
+  return palettes.filter((palette) => {
+    if (type && palette.type !== _type) return false;
+    if (number && palette.number !== number) return false;
+    if (provider && palette.provider !== _provider) return false;
+    if (name && palette.name.toLowerCase() !== _name) return false;
+    return true;
+  });
+}
+
+/**
+ * @description Return the names of the palette providers.
+ * @returns {string[]} - Palette providers
+ */
+export const getPaletteProviders = (): Provider[] => Object.keys(Provider).map((key) => Provider[key as keyof typeof Provider]);
+
+/**
+ * @description Return the names of the palette types.
+ * @returns {string[]} - Palette types
+ */
+export const getPaletteTypes = (): PaletteType[] => Object.keys(PaletteType).map((key) => PaletteType[key as keyof typeof PaletteType]);
+
+/**
+ * @description Return the names of the palettes.
+ * @returns {string[]} - Palette names
+ */
+export const getPaletteNames = (): string[] => [...new Set(palettes.map((palette) => palette.name))];
